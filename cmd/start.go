be71696a62d8db4253d4518b9d2243cfcff482ee
nedy <ncodes@Kennedys-MacBook-Pro.local>
2018-03-22 18:03:40 +0100

    Now using cobra for CLI management

---
+++ cmd/start.go
@@ -0,0 +1,68 @@
+// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/ellcrys/garagecoin/components"
+	"github.com/ellcrys/garagecoin/protocols/inception"
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+)
+
+var log *zap.SugaredLogger
+
+func init() {
+	log = components.NewLogger("/peer")
+}
+
+// startCmd represents the start command
+var startCmd = &cobra.Command{
+	Use:   "start",
+	Short: "Start the peer",
+	Long:  `Start the peer`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		// create the peer
+		peer, err := components.NewPeer(4500, 10)
+		if err != nil {
+			log.Fatalf("failed to create peer")
+		}
+
+		log.Infof("Address is %s", peer.GetAddress())
+
+		// set protocol version and handler
+		peer.SetProtocolHandler(inception.NewInception("/inception/0.0.1"))
+		fmt.Println(peer.GetAddress())
+
+		// cause main thread to wait for peer
+		peer.Wait()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(startCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// startCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// startCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
-
 	"go.uber.org/zap"
 
+	"github.com/ellcrys/garagecoin/cmd"
 	"github.com/ellcrys/garagecoin/components"
-	"github.com/ellcrys/garagecoin/protocols/inception"
 )
 
 var log *zap.SugaredLogger
@@ -16,21 +14,6 @@ func init() {
 }
 
 func main() {
-
 	log.Infof("Garagecoin node started")
-
-	// create the peer
-	peer, err := components.NewPeer(4500, 10)
-	if err != nil {
-		log.Fatalf("failed to create peer")
-	}
-
-	log.Infof("=> Address: %s", peer.GetAddress())
-
-	// set protocol version and handler
-	peer.SetProtocolHandler(inception.NewInception("/inception/0.0.1"))
-	fmt.Println(peer.GetAddress())
-
-	// cause main thread to wait for peer
-	peer.Wait()
+	cmd.Execute()
 }
