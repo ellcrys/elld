2a9159f83082d087a2e2a2c691760e6173a85f5d
nedy <ncodes@Kennedys-MacBook-Pro.local>
2018-03-24 16:03:43 +0100

    Added new `HandleHandshake` method to protocol interface. Implementation will be included in `op_handshake.go` which is a part of the `Inception` struct.

--- modules/types/protocol.go
+++ modules/types/protocol.go
@@ -1,11 +1,14 @@
 package types
 
-import net "github.com/libp2p/go-libp2p-net"
+import (
+	net "github.com/libp2p/go-libp2p-net"
+	protocol "github.com/libp2p/go-libp2p-protocol"
+)
 
 // StreamProtocol represents a protocol
 type StreamProtocol interface {
 	GetVersion() string
 	GetCodeName() string
 	Handle(net.Stream)
-	HandleHandshakeMsg()
+	HandleHandshake(*Message, protocol.ID, net.Conn)
 }
