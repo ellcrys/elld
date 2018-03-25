35577c149c15bf75c97e0f2340eed101470a70dc
nedy <ncodes@Kennedys-MacBook-Pro.local>
2018-03-25 15:48:50 +0100

    Added `WaitThenReadStream` and `WaitThenReadHexStream` to read a stream sometime in the future.

--- modules/util/io.go
+++ modules/util/io.go
@@ -5,9 +5,15 @@ import (
 	"fmt"
 	"io"
 	"strings"
+	"time"
 
 	"github.com/ellcrys/garagecoin/modules/types"
 	net "github.com/libp2p/go-libp2p-net"
+)
+
+var (
+	// WaitTimeBeforeRead is the amount of time to wait before reading a stream
+	WaitTimeBeforeRead = 10 * time.Millisecond
 )
 
 // ReadHexStream reads and decodes the content of a stream if the context is hex encoded
@@ -63,4 +69,20 @@ func ReadMessageFromStream(s net.Stream) (*types.Message, error) {
 	}
 
 	return m, nil
+}
+
+// WaitThenReadStream reads the content of a stream after a period of time has passed
+func WaitThenReadStream(waitTime time.Duration, s net.Stream, cb func(error, []byte)) {
+	time.AfterFunc(waitTime, func() {
+		bs, err := ReadStream(s)
+		cb(err, bs)
+	})
+}
+
+// WaitThenReadHexStream reads the content of a stream after a period of time has passed
+func WaitThenReadHexStream(waitTime time.Duration, s net.Stream, cb func(error, []byte)) {
+	time.AfterFunc(waitTime, func() {
+		bs, err := ReadHexStream(s)
+		cb(err, bs)
+	})
 }
