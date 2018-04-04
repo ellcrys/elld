232584649ce1c04b9bbae46571a94ff914536d59
nedy <ncodes@Kennedys-MacBook-Pro.local>
2018-04-04 10:32:35 +0100

    Added tests for util package

---
+++ modules/util/common_test.go
@@ -0,0 +1,19 @@
+package util_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/ellcrys/gcoin/modules/util"
+)
+
+var _ = Describe("Common", func() {
+	Describe(".StructToBytes", func() {
+		It("should return []byte{123, 34, 78, 97, 109, 101, 34, 58, 34, 98, 101, 110, 34, 125} bytes", func() {
+			s := struct{ Name string }{Name: "ben"}
+			expected := []byte{123, 34, 78, 97, 109, 101, 34, 58, 34, 98, 101, 110, 34, 125}
+			bs := StructToBytes(s)
+			Expect(bs).To(Equal(expected))
+		})
+	})
+})
