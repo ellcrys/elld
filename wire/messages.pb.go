744fbb7bfc07e6f090cba409f90fa31259d6e5b2
Kennedy Idialu <kennedyidialu@gmail.com>
2018-04-29 09:13:54 +0100

    Removed `HandshakeAck`. `Handshake` can me used for acknowledgement.

--- wire/messages.pb.go
+++ wire/messages.pb.go
@@ -9,7 +9,6 @@
 
 	It has these top-level messages:
 		Handshake
-		HandshakeAck
 		GetAddr
 		Addr
 		Address
@@ -64,30 +63,6 @@ func (m *Handshake) GetSig() []byte {
 	return nil
 }
 
-// HandshakeAck is a response to Handshake message
-type HandshakeAck struct {
-	SubVersion string `protobuf:"bytes,1,opt,name=subVersion,proto3" json:"subVersion,omitempty"`
-	Sig        []byte `protobuf:"bytes,2,opt,name=sig,proto3" json:"sig,omitempty"`
-}
-
-func (m *HandshakeAck) Reset()                    { *m = HandshakeAck{} }
-func (*HandshakeAck) ProtoMessage()               {}
-func (*HandshakeAck) Descriptor() ([]byte, []int) { return fileDescriptorMessages, []int{1} }
-
-func (m *HandshakeAck) GetSubVersion() string {
-	if m != nil {
-		return m.SubVersion
-	}
-	return ""
-}
-
-func (m *HandshakeAck) GetSig() []byte {
-	if m != nil {
-		return m.Sig
-	}
-	return nil
-}
-
 // GetAddr is used to request for addresses from other peers
 type GetAddr struct {
 	Sig []byte `protobuf:"bytes,1,opt,name=sig,proto3" json:"sig,omitempty"`
@@ -95,7 +70,7 @@ type GetAddr struct {
 
 func (m *GetAddr) Reset()                    { *m = GetAddr{} }
 func (*GetAddr) ProtoMessage()               {}
-func (*GetAddr) Descriptor() ([]byte, []int) { return fileDescriptorMessages, []int{2} }
+func (*GetAddr) Descriptor() ([]byte, []int) { return fileDescriptorMessages, []int{1} }
 
 func (m *GetAddr) GetSig() []byte {
 	if m != nil {
@@ -112,7 +87,7 @@ type Addr struct {
 
 func (m *Addr) Reset()                    { *m = Addr{} }
 func (*Addr) ProtoMessage()               {}
-func (*Addr) Descriptor() ([]byte, []int) { return fileDescriptorMessages, []int{3} }
+func (*Addr) Descriptor() ([]byte, []int) { return fileDescriptorMessages, []int{2} }
 
 func (m *Addr) GetAddresses() []*Address {
 	if m != nil {
@@ -135,7 +110,7 @@ type Address struct {
 
 func (m *Address) Reset()                    { *m = Address{} }
 func (*Address) ProtoMessage()               {}
-func (*Address) Descriptor() ([]byte, []int) { return fileDescriptorMessages, []int{4} }
+func (*Address) Descriptor() ([]byte, []int) { return fileDescriptorMessages, []int{3} }
 
 func (m *Address) GetAddress() string {
 	if m != nil {
@@ -158,7 +133,7 @@ type Ping struct {
 
 func (m *Ping) Reset()                    { *m = Ping{} }
 func (*Ping) ProtoMessage()               {}
-func (*Ping) Descriptor() ([]byte, []int) { return fileDescriptorMessages, []int{5} }
+func (*Ping) Descriptor() ([]byte, []int) { return fileDescriptorMessages, []int{4} }
 
 func (m *Ping) GetSig() []byte {
 	if m != nil {
@@ -174,7 +149,7 @@ type Pong struct {
 
 func (m *Pong) Reset()                    { *m = Pong{} }
 func (*Pong) ProtoMessage()               {}
-func (*Pong) Descriptor() ([]byte, []int) { return fileDescriptorMessages, []int{6} }
+func (*Pong) Descriptor() ([]byte, []int) { return fileDescriptorMessages, []int{5} }
 
 func (m *Pong) GetSig() []byte {
 	if m != nil {
@@ -185,7 +160,6 @@ func (m *Pong) GetSig() []byte {
 
 func init() {
 	proto.RegisterType((*Handshake)(nil), "wire.Handshake")
-	proto.RegisterType((*HandshakeAck)(nil), "wire.HandshakeAck")
 	proto.RegisterType((*GetAddr)(nil), "wire.GetAddr")
 	proto.RegisterType((*Addr)(nil), "wire.Addr")
 	proto.RegisterType((*Address)(nil), "wire.Address")
@@ -200,33 +174,6 @@ func (this *Handshake) Equal(that interface{}) bool {
 	that1, ok := that.(*Handshake)
 	if !ok {
 		that2, ok := that.(Handshake)
-		if ok {
-			that1 = &that2
-		} else {
-			return false
-		}
-	}
-	if that1 == nil {
-		return this == nil
-	} else if this == nil {
-		return false
-	}
-	if this.SubVersion != that1.SubVersion {
-		return false
-	}
-	if !bytes.Equal(this.Sig, that1.Sig) {
-		return false
-	}
-	return true
-}
-func (this *HandshakeAck) Equal(that interface{}) bool {
-	if that == nil {
-		return this == nil
-	}
-
-	that1, ok := that.(*HandshakeAck)
-	if !ok {
-		that2, ok := that.(HandshakeAck)
 		if ok {
 			that1 = &that2
 		} else {
@@ -388,17 +335,6 @@ func (this *Handshake) GoString() string {
 	s = append(s, "}")
 	return strings.Join(s, "")
 }
-func (this *HandshakeAck) GoString() string {
-	if this == nil {
-		return "nil"
-	}
-	s := make([]string, 0, 6)
-	s = append(s, "&wire.HandshakeAck{")
-	s = append(s, "SubVersion: "+fmt.Sprintf("%#v", this.SubVersion)+",\n")
-	s = append(s, "Sig: "+fmt.Sprintf("%#v", this.Sig)+",\n")
-	s = append(s, "}")
-	return strings.Join(s, "")
-}
 func (this *GetAddr) GoString() string {
 	if this == nil {
 		return "nil"
@@ -472,36 +408,6 @@ func (m *Handshake) Marshal() (dAtA []byte, err error) {
 }
 
 func (m *Handshake) MarshalTo(dAtA []byte) (int, error) {
-	var i int
-	_ = i
-	var l int
-	_ = l
-	if len(m.SubVersion) > 0 {
-		dAtA[i] = 0xa
-		i++
-		i = encodeVarintMessages(dAtA, i, uint64(len(m.SubVersion)))
-		i += copy(dAtA[i:], m.SubVersion)
-	}
-	if len(m.Sig) > 0 {
-		dAtA[i] = 0x12
-		i++
-		i = encodeVarintMessages(dAtA, i, uint64(len(m.Sig)))
-		i += copy(dAtA[i:], m.Sig)
-	}
-	return i, nil
-}
-
-func (m *HandshakeAck) Marshal() (dAtA []byte, err error) {
-	size := m.Size()
-	dAtA = make([]byte, size)
-	n, err := m.MarshalTo(dAtA)
-	if err != nil {
-		return nil, err
-	}
-	return dAtA[:n], nil
-}
-
-func (m *HandshakeAck) MarshalTo(dAtA []byte) (int, error) {
 	var i int
 	_ = i
 	var l int
@@ -681,20 +587,6 @@ func (m *Handshake) Size() (n int) {
 	return n
 }
 
-func (m *HandshakeAck) Size() (n int) {
-	var l int
-	_ = l
-	l = len(m.SubVersion)
-	if l > 0 {
-		n += 1 + l + sovMessages(uint64(l))
-	}
-	l = len(m.Sig)
-	if l > 0 {
-		n += 1 + l + sovMessages(uint64(l))
-	}
-	return n
-}
-
 func (m *GetAddr) Size() (n int) {
 	var l int
 	_ = l
@@ -772,17 +664,6 @@ func (this *Handshake) String() string {
 		return "nil"
 	}
 	s := strings.Join([]string{`&Handshake{`,
-		`SubVersion:` + fmt.Sprintf("%v", this.SubVersion) + `,`,
-		`Sig:` + fmt.Sprintf("%v", this.Sig) + `,`,
-		`}`,
-	}, "")
-	return s
-}
-func (this *HandshakeAck) String() string {
-	if this == nil {
-		return "nil"
-	}
-	s := strings.Join([]string{`&HandshakeAck{`,
 		`SubVersion:` + fmt.Sprintf("%v", this.SubVersion) + `,`,
 		`Sig:` + fmt.Sprintf("%v", this.Sig) + `,`,
 		`}`,
@@ -876,116 +757,6 @@ func (m *Handshake) Unmarshal(dAtA []byte) error {
 		}
 		if fieldNum <= 0 {
 			return fmt.Errorf("proto: Handshake: illegal tag %d (wire type %d)", fieldNum, wire)
-		}
-		switch fieldNum {
-		case 1:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field SubVersion", wireType)
-			}
-			var stringLen uint64
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowMessages
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				stringLen |= (uint64(b) & 0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
-				return ErrInvalidLengthMessages
-			}
-			postIndex := iNdEx + intStringLen
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.SubVersion = string(dAtA[iNdEx:postIndex])
-			iNdEx = postIndex
-		case 2:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Sig", wireType)
-			}
-			var byteLen int
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowMessages
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				byteLen |= (int(b) & 0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			if byteLen < 0 {
-				return ErrInvalidLengthMessages
-			}
-			postIndex := iNdEx + byteLen
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.Sig = append(m.Sig[:0], dAtA[iNdEx:postIndex]...)
-			if m.Sig == nil {
-				m.Sig = []byte{}
-			}
-			iNdEx = postIndex
-		default:
-			iNdEx = preIndex
-			skippy, err := skipMessages(dAtA[iNdEx:])
-			if err != nil {
-				return err
-			}
-			if skippy < 0 {
-				return ErrInvalidLengthMessages
-			}
-			if (iNdEx + skippy) > l {
-				return io.ErrUnexpectedEOF
-			}
-			iNdEx += skippy
-		}
-	}
-
-	if iNdEx > l {
-		return io.ErrUnexpectedEOF
-	}
-	return nil
-}
-func (m *HandshakeAck) Unmarshal(dAtA []byte) error {
-	l := len(dAtA)
-	iNdEx := 0
-	for iNdEx < l {
-		preIndex := iNdEx
-		var wire uint64
-		for shift := uint(0); ; shift += 7 {
-			if shift >= 64 {
-				return ErrIntOverflowMessages
-			}
-			if iNdEx >= l {
-				return io.ErrUnexpectedEOF
-			}
-			b := dAtA[iNdEx]
-			iNdEx++
-			wire |= (uint64(b) & 0x7F) << shift
-			if b < 0x80 {
-				break
-			}
-		}
-		fieldNum := int32(wire >> 3)
-		wireType := int(wire & 0x7)
-		if wireType == 4 {
-			return fmt.Errorf("proto: HandshakeAck: wiretype end group for non-group")
-		}
-		if fieldNum <= 0 {
-			return fmt.Errorf("proto: HandshakeAck: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
 		case 1:
@@ -1630,23 +1401,22 @@ var (
 func init() { proto.RegisterFile("wire/messages.proto", fileDescriptorMessages) }
 
 var fileDescriptorMessages = []byte{
-	// 273 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x2e, 0xcf, 0x2c, 0x4a,
-	0xd5, 0xcf, 0x4d, 0x2d, 0x2e, 0x4e, 0x4c, 0x4f, 0x2d, 0xd6, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17,
-	0x62, 0x01, 0x09, 0x2a, 0xd9, 0x72, 0x71, 0x7a, 0x24, 0xe6, 0xa5, 0x14, 0x67, 0x24, 0x66, 0xa7,
-	0x0a, 0xc9, 0x71, 0x71, 0x15, 0x97, 0x26, 0x85, 0xa5, 0x16, 0x15, 0x67, 0xe6, 0xe7, 0x49, 0x30,
-	0x2a, 0x30, 0x6a, 0x70, 0x06, 0x21, 0x89, 0x08, 0x09, 0x70, 0x31, 0x17, 0x67, 0xa6, 0x4b, 0x30,
-	0x29, 0x30, 0x6a, 0xf0, 0x04, 0x81, 0x98, 0x4a, 0x0e, 0x5c, 0x3c, 0x70, 0xed, 0x8e, 0xc9, 0xd9,
-	0x64, 0x98, 0x20, 0xcd, 0xc5, 0xee, 0x9e, 0x5a, 0xe2, 0x98, 0x92, 0x52, 0x04, 0x93, 0x64, 0x44,
-	0x48, 0xba, 0x72, 0xb1, 0x80, 0x65, 0xb4, 0xb9, 0x38, 0x13, 0x53, 0x52, 0x8a, 0x52, 0x8b, 0x8b,
-	0x53, 0x8b, 0x25, 0x18, 0x15, 0x98, 0x35, 0xb8, 0x8d, 0x78, 0xf5, 0x40, 0xee, 0xd7, 0x73, 0x84,
-	0x08, 0x07, 0x21, 0xe4, 0xb1, 0xd8, 0xe1, 0xc8, 0xc5, 0x0e, 0x55, 0x27, 0x24, 0xc1, 0xc5, 0x0e,
-	0x55, 0x09, 0x75, 0x1d, 0x8c, 0x2b, 0x24, 0xc3, 0xc5, 0x59, 0x92, 0x99, 0x9b, 0x5a, 0x5c, 0x92,
-	0x98, 0x5b, 0x00, 0xd6, 0xcc, 0x1c, 0x84, 0x10, 0x50, 0x92, 0xe0, 0x62, 0x09, 0xc8, 0xcc, 0x4b,
-	0x07, 0x19, 0x1e, 0x8c, 0x70, 0x63, 0x70, 0x66, 0x3a, 0x58, 0x26, 0x1f, 0x9b, 0x8c, 0x93, 0xce,
-	0x85, 0x87, 0x72, 0x0c, 0x37, 0x1e, 0xca, 0x31, 0x7c, 0x78, 0x28, 0xc7, 0xd8, 0xf0, 0x48, 0x8e,
-	0x71, 0xc5, 0x23, 0x39, 0xc6, 0x13, 0x8f, 0xe4, 0x18, 0x2f, 0x3c, 0x92, 0x63, 0x7c, 0xf0, 0x48,
-	0x8e, 0xf1, 0xc5, 0x23, 0x39, 0x86, 0x0f, 0x8f, 0xe4, 0x18, 0x27, 0x3c, 0x96, 0x63, 0x48, 0x62,
-	0x03, 0x47, 0x8b, 0x31, 0x20, 0x00, 0x00, 0xff, 0xff, 0x4c, 0x7b, 0x8f, 0xe9, 0xad, 0x01, 0x00,
-	0x00,
+	// 262 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x6c, 0x90, 0xb1, 0x4a, 0xc4, 0x40,
+	0x10, 0x86, 0x33, 0x5e, 0x30, 0x64, 0x54, 0x90, 0xd8, 0x2c, 0x28, 0x43, 0x48, 0x15, 0x50, 0x22,
+	0x68, 0x6d, 0x11, 0x41, 0xb4, 0x94, 0x1c, 0xd8, 0xef, 0xb1, 0x4b, 0x5c, 0x24, 0xc9, 0xb1, 0x13,
+	0xb1, 0xf5, 0x11, 0x7c, 0x0c, 0x1f, 0xc5, 0xf2, 0x4a, 0x4b, 0xb3, 0x36, 0x96, 0xf7, 0x08, 0x92,
+	0x98, 0x23, 0x16, 0xd7, 0xed, 0x7e, 0xdf, 0xec, 0xfe, 0x3f, 0x83, 0x47, 0x2f, 0xc6, 0xea, 0xf3,
+	0x4a, 0x33, 0xcb, 0x52, 0x73, 0xb6, 0xb4, 0x4d, 0xdb, 0x44, 0x7e, 0x0f, 0x93, 0x2b, 0x0c, 0xef,
+	0x64, 0xad, 0xf8, 0x51, 0x3e, 0xe9, 0x88, 0x10, 0xf9, 0x79, 0xf1, 0xa0, 0x2d, 0x9b, 0xa6, 0x16,
+	0x10, 0x43, 0x1a, 0x16, 0xff, 0x48, 0x74, 0x88, 0x33, 0x36, 0xa5, 0xd8, 0x89, 0x21, 0xdd, 0x2f,
+	0xfa, 0x63, 0x72, 0x8c, 0xc1, 0xad, 0x6e, 0x73, 0xa5, 0xec, 0x46, 0xc2, 0x24, 0x6f, 0xd0, 0x1f,
+	0xcc, 0x29, 0x86, 0x52, 0x29, 0xab, 0x99, 0x35, 0x0b, 0x88, 0x67, 0xe9, 0xde, 0xc5, 0x41, 0xd6,
+	0xa7, 0x67, 0xf9, 0x1f, 0x2e, 0x26, 0xbf, 0x25, 0x23, 0xc7, 0x60, 0x9c, 0x8b, 0x04, 0x06, 0xe3,
+	0xe4, 0xd8, 0x6e, 0x73, 0x8d, 0x4e, 0x30, 0x6c, 0x4d, 0xa5, 0xb9, 0x95, 0xd5, 0x72, 0x78, 0x3c,
+	0x2b, 0x26, 0x90, 0x08, 0xf4, 0xef, 0x4d, 0x5d, 0xf6, 0x9f, 0xcf, 0xa7, 0x8e, 0x73, 0x53, 0x0e,
+	0xa6, 0xd9, 0x66, 0xae, 0xcf, 0x56, 0x1d, 0x79, 0x9f, 0x1d, 0x79, 0xeb, 0x8e, 0xe0, 0xd5, 0x11,
+	0xbc, 0x3b, 0x82, 0x0f, 0x47, 0xb0, 0x72, 0x04, 0x5f, 0x8e, 0xe0, 0xc7, 0x91, 0xb7, 0x76, 0x04,
+	0x6f, 0xdf, 0xe4, 0x2d, 0x76, 0x87, 0xa5, 0x5e, 0xfe, 0x06, 0x00, 0x00, 0xff, 0xff, 0x6a, 0x9f,
+	0x8b, 0x6c, 0x6b, 0x01, 0x00, 0x00,
 }
