14d48655abc76c89d86e7cf2fc0a093609d392ee
Sodimu Segun Michael <princesegzy01@yahoo.com>
2018-05-15 22:24:22 +0100

    Change rlpHash to Asn1 Hash for Block Hashing

--- block/block.go
+++ block/block.go
@@ -1,11 +1,11 @@
 package block
 
 import (
+	"crypto/sha256"
+	"encoding/asn1"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto/sha3"
-	"github.com/ethereum/go-ethereum/rlp"
 )
 
 //Block  Construct
@@ -22,23 +22,31 @@ type Block struct {
 	PowResult      string
 }
 
-// HashNoNonce returns the hash which is used as input for the proof-of-work search.
-func (h *Block) HashNoNonce() common.Hash {
-	return rlpHash([]interface{}{
-		h.HashPrevBlock,
-		h.HashMerkleRoot,
-		h.Difficulty,
-		// h.Number,
-		h.Time,
-		// h.Extra,
-	})
+type asn1BlockNoNonce struct {
+	HashPrevBlock  string `asn1:"utf8" json:"hashPrevBlock"`
+	HashMerkleRoot string `asn1:"utf8" json:"hashMerkleRoot"`
+	Difficulty     string `asn1:"utf8" json:"difficulty"`
+	Number         int64  `json:"number"`
+	Time           string `json:"string"`
 }
 
-func rlpHash(x interface{}) (h common.Hash) {
-	hw := sha3.NewKeccak256()
-	rlp.Encode(hw, x)
-	hw.Sum(h[:0])
-	return h
+// HashNoNonce returns the hash which is used as input for the proof-of-work search.
+func (h *Block) HashNoNonce() common.Hash {
+
+	asn1B := asn1BlockNoNonce{
+		HashPrevBlock:  h.HashPrevBlock,
+		HashMerkleRoot: h.HashMerkleRoot,
+		Difficulty:     h.Difficulty,
+		// Number:         h.Number,
+		Time: h.Time,
+	}
+
+	result, err := asn1.Marshal(asn1B)
+	if err != nil {
+		panic(err)
+	}
+
+	return sha256.Sum256(result)
 }
 
 //GetGenesisDifficulty get the genesis block difficulty
